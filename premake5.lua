78aeb638c1fe4dc9e7675436eddf21a2a0f0f246
Steve Olsen <steve.olsen@gmail.com>
2017-09-09 13:16:18 -0500

    restructure directory and switch to premake

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+-- @TODO
+-- Enable SIMD
+-- Maybe make netcode and realiable submodules and just link to their premake projects.
+-- Maybe even do the same with raylib and glfw3...
+-- I think I probably don't link to sodium on linux, just make sure it's installed? Or do I bundle it too...
+
+workspace "WizardGame"
+   configurations { "Debug", "Release" }
+   architecture "x86_64"
+   location "build"
+   language "C"
+   symbols "On"
+   includedirs { "3rdparty/include" }
+
+   filter "system:windows"
+      system "windows"
+      libdirs { "3rdparty/lib/windows" }
+
+   filter "system:macosx"
+      system "macosx"
+
+   filter "system:linux"
+      system "linux"
+
+project "WizardClient"
+   kind "ConsoleApp"
+   files { "src/wizard_client.c", "3rdparty/include/netcode.c" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      links { "glfw3", "raylib", "sodium-debug" }
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "Speed"
+      links { "glfw3", "raylib", "sodium-release" }
+
+project "WizardServer"
+   kind "ConsoleApp"
+   files { "src/wizard_server.c", "3rdparty/include/netcode.c" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      links { "sodium-debug" }
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "Speed"
+      links { "sodium-release" }
+   
