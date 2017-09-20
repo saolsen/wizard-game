84d0c0b5534d5a2f126152f99db97359245347d7
Steve Olsen <steve.olsen@gmail.com>
2017-09-20 10:15:59 -0500

    Don't install libsodium as it's included now.

--- tools/linux/install_vsts_deps.sh
+++ tools/linux/install_vsts_deps.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y musl-tools
-
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz && \
-    tar -zxvf libsodium-1.0.10.tar.gz && \
-    cd libsodium-1.0.10 && \
-    ./configure && \
-    make CC=musl-gcc && make CC=musl-gcc check && \
-    make CC=musl-gcc install && \
-    cd .. && \
-    rm -rf libsodium* && \
-    ldconfig /
