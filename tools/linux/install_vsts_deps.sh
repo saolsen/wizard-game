3173a372b014f93a24af31cbdb6bad0b791cc06e
Stephen Olsen <steve.olsen@gmail.com>
2017-09-19 22:52:32 -0500

    libsodium

--- tools/linux/install_vsts_deps.sh
+++ tools/linux/install_vsts_deps.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y musl-tools
+
+RUN wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz && \
+    tar -zxvf libsodium-1.0.10.tar.gz && \
+    cd libsodium-1.0.10 && \
+    ./configure && \
+    make && make check && \
+    make install && \
+    cd .. && \
+    rm -rf libsodium* && \
+    ldconfig /
