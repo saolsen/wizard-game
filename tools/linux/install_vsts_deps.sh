39228f18d38b0b1663f6795f9fd3f6f5b6a9b7ea
Stephen Olsen <steve.olsen@gmail.com>
2017-09-19 22:55:46 -0500

    this is not docker

--- tools/linux/install_vsts_deps.sh
+++ tools/linux/install_vsts_deps.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get install -y musl-tools
 
-RUN wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz && \
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz && \
     tar -zxvf libsodium-1.0.10.tar.gz && \
     cd libsodium-1.0.10 && \
     ./configure && \
